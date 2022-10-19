c792c75ec0e683be600f6a8fd55ba7ca49a84a22
Adrian1Arceredillo <arceredillo.adrian@uni.eus>
2022-10-19 19:19:30 +0200

    .

--- Errepasoa/mendienfitxategiak/src/main/java/dambi/mainklaseak/Irakurketak.java
+++ Errepasoa/mendienfitxategiak/src/main/java/dambi/mainklaseak/Irakurketak.java
@@ -9,13 +9,15 @@ public class Irakurketak {
     public static void main(String[] args) {
 
         Mendiak mendiak = new Mendiak();
-        Csva csva = new Csva("data/Mendiakkkk.csv");
+        // Csva csva = new Csva("data/Mendiakkkk.csv");
+        Csva csva = new Csva("mendienfitxategiak/data/Mendiak.csv");    //copiar path desde la venta de workspace
         mendiak = csva.irakurri();
         if (mendiak != null) {
             System.out.println(csva.strFileIn + " fitxategian mendi hauek daude: ");
             System.out.println(mendiak);
         }
-        Xmla xmla = new Xmla("data/Hirumendiiii.xml");
+        // Xmla xmla = new Xmla("data/Hirumendiiii.xml");
+        Xmla xmla = new Xmla("Hirumendi.xml");
         mendiak = xmla.irakurri();
         if (mendiak != null) {
             System.out.println(xmla.strFileIn + " fitxategian mendi hauek daude: ");
